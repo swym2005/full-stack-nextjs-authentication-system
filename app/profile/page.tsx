7439e7d5ec24eb2a038cc7e513b9e494dbeb1588
Swayam Dalal <dalalswayam459@gmail.com>
2026-03-28 15:14:43 +0530

    Added the middleware part

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,9 +1,48 @@
+"use client"
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast, { Toaster } from "react-hot-toast";
+import { useState } from "react";
+import Link from "next/link";
+
+
 export default function ProfilePage() {
+
+    const router = useRouter();
+
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout")
+            toast.success("Logout Success");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error.message);
+            toast.error(error.message);
+            
+        }
+    }
+
+    const renderUserDetails = async () => {
+            const response = await axios.get('/api/users/me');
+            console.log(response.data);
+            // response has a field data in which we name a field data which contains _id so...
+            setData(response.data.data._id);
+    }
+
+    const [data, setData] = useState("nothing");
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page </p>
+            <h2>{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
+            <hr />
+            <button onClick={logout} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold
+            py-2 px-4 rounded">Logout</button>
+             <button onClick={renderUserDetails} className="bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold
+            py-2 px-4 rounded">Go to UserDetails</button>
+            <Toaster position="bottom-right"/>
         </div>
     )
 }
---
+++ proxy.ts
@@ -0,0 +1,46 @@
+// ****************************
+// This is a middleware file Jisko Proxy kahenge.
+// ****************************
+
+
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function proxy(request: NextRequest){
+// Grab the paths..
+const path = request.nextUrl.pathname;
+
+// Defining that these paths are public paths..
+const isPublicPath = path === '/login' || path === '/signup';
+
+// Grabbing the token from the user
+const token = request.cookies.get("token")?.value || "";
+
+// So the logic is gonna be like if you are having a token and you're trying to access the public paths
+// then you should be redirected somewhere.
+
+if(isPublicPath && token)
+{
+    return NextResponse.redirect(new URL('/', request.nextUrl))
+}
+
+// If you dont have the token and not on the Public path and trying to access the private ones then go to login..
+if(!isPublicPath && !token)
+{
+    return NextResponse.redirect(new URL('/login', request.nextUrl));
+}
+
+
+
+
+}
+
+// The matching portion
+export const config = {
+    matcher: [
+        '/',
+        '/profile',
+        '/login',
+        '/signup'
+    ]
+}
